9f93ab20252dac1fef6f2471fdb0a3f38526a417
naveenpadeti <naveenpadeti123@gmail.com>
2025-07-23 10:02:47 +0530

    my commit for deploy skill 3

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/klu2300031922/', // Set the base path for the application
+});
